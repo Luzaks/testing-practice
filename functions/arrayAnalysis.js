6d2f7c36c1fe7d02c413ed38e95ba41a725e8266
Luzaks <kanemekanik@gmail.com>
2020-06-03 14:32:25 -0500

    Feat: Add functions for analyzing an array. Test: Add tests for analyzing the array function.

---
+++ functions/arrayAnalysis.js
@@ -0,0 +1,25 @@
+function arrayAnalysis(array) {
+    const length = array.length;
+    let average = 0;
+    let minNum = 0;
+    let maxNum = 0;
+    let sum = 0;
+    if (length !== 0) {
+        minNum = array[0];
+        maxNum = array[0];
+        for(let i = 0 ; i < length ; i += 1) {
+            if (minNum > array[i]) minNum = array[i];
+            if (maxNum < array[i]) maxNum = array[i];
+            sum += array[i];
+        }
+        average = sum / length;
+    }
+    return {
+        average: average,
+        min: minNum,
+        max: maxNum,
+        length: length,
+    }
+}
+
+export default arrayAnalysis;
