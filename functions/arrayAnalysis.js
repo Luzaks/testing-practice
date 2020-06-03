e85bf7b45ed2a85e5951de9b1b724586ee2240c2
MarioDena <enrique.dn@outlook.com>
2020-06-03 14:39:36 -0500

    [Fix] Corrected linter errors localy and added linter files

--- functions/arrayAnalysis.js
+++ functions/arrayAnalysis.js
@@ -1,25 +1,26 @@
+/* eslint-disable prefer-destructuring */
 function arrayAnalysis(array) {
-    const length = array.length;
-    let average = 0;
-    let minNum = 0;
-    let maxNum = 0;
-    let sum = 0;
-    if (length !== 0) {
-        minNum = array[0];
-        maxNum = array[0];
-        for(let i = 0 ; i < length ; i += 1) {
-            if (minNum > array[i]) minNum = array[i];
-            if (maxNum < array[i]) maxNum = array[i];
-            sum += array[i];
-        }
-        average = sum / length;
+  const { length } = array;
+  let average = 0;
+  let minNum = 0;
+  let maxNum = 0;
+  let sum = 0;
+  if (length !== 0) {
+    minNum = array[0];
+    maxNum = array[0];
+    for (let i = 0; i < length; i += 1) {
+      if (minNum > array[i]) minNum = array[i];
+      if (maxNum < array[i]) maxNum = array[i];
+      sum += array[i];
     }
-    return {
-        average: average,
-        min: minNum,
-        max: maxNum,
-        length: length,
-    }
+    average = sum / length;
+  }
+  return {
+    average,
+    min: minNum,
+    max: maxNum,
+    length,
+  };
 }
 
 export default arrayAnalysis;
--- functions/caesarCipher.js
+++ functions/caesarCipher.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 const caesar = (string, shift) => {
   let result = '';
 
